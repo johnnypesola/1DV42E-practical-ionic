ff9a3946e0f4e619aec0a7eec0edeab72851fb14
Johnny Pesola <johnny@pesola.eu>
2016-04-20 19:32:51 +0200

    Furnituring details view done

--- src/js/controllers/furnituring/furnituring-ctrl.js
+++ src/js/controllers/furnituring/furnituring-ctrl.js
@@ -48,6 +48,8 @@ angular.module( 'BookingSystem.furnituring',
     .controller( 'FurnituringDetailsCtrl', [ '$rootScope', '$scope', '$stateParams', '$state', 'Furnituring', ( $rootScope, $scope, $stateParams, $state, Furnituring ) => {
 
       /* Init vars */
+      $scope.editMode = false;
+      $scope.furnituringBackup = {};
 
       /* Private methods START */
 
@@ -69,12 +71,87 @@ angular.module( 'BookingSystem.furnituring',
         });
 
         $scope.furnituring = furnituring;
-
       };
 
       /* Private Methods END */
 
       /* Public Methods START */
+
+      $scope.startEditMode = function () {
+        const $scope = this;
+
+        $scope.isEditMode = true;
+
+        // Make backup of data if in editMode.
+        $scope.furnituringBackup = angular.copy( $scope.furnituring );
+      };
+
+      $scope.endEditMode = function () {
+        const $scope = this;
+
+        $scope.isEditMode = false;
+      };
+
+      $scope.abortEditMode = function() {
+        const $scope = this;
+
+        $scope.isEditMode = false;
+        $scope.furnituring = $scope.furnituringBackup;
+      };
+
+      $scope.saveFurnituring = function() {
+
+        const $scope = this;
+
+        // Save furnituring
+        Furnituring.save(
+          {
+            FurnituringId: $stateParams.furnituringId,
+            Name: $scope.furnituring.Name
+          }
+        ).$promise
+
+          // If everything went ok
+          .then( ( response ) => {
+
+            $scope.endEditMode();
+
+            $rootScope.FlashMessage = {
+              type: 'success',
+              message: 'Möbleringen "' + $scope.furnituring.Name + '" sparades med ett lyckat resultat'
+            };
+
+            // Something went wrong
+          }).catch( ( response ) => {
+
+            // If there there was a foreign key reference
+            if ( response.status === 409 ){
+              $rootScope.FlashMessage = {
+                type: 'error',
+                message: 'Det finns redan en möblering som heter "' + $scope.furnituring.Name +
+                '". Två möbleringar kan inte heta lika.'
+              };
+            }
+
+            // If there was a problem with the in-data
+            else if ( response.status === 400 || response.status === 500 ){
+              $rootScope.FlashMessage = {
+                type: 'error',
+                message: 'Ett oväntat fel uppstod när möbleringen skulle sparas'
+              };
+            }
+
+            // If the entry was not found
+            if ( response.status === 404 ) {
+              $rootScope.FlashMessage = {
+                type: 'error',
+                message: 'Möbleringen "' + $scope.furnituring.Name + '" existerar inte längre. Hann kanske någon radera den?'
+              };
+
+              history.back();
+            }
+          });
+      };
 
       /* Public Methods END */
 
