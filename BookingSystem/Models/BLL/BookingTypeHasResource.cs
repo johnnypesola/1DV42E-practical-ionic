8bde7b247d163849ddea172d583c573ef7c63f9a
Johnny Pesola <johnny@pesola.eu>
2016-04-28 17:54:05 +0200

    BookingTypeHasMeal REST API done

---
+++ BookingSystem/Models/BLL/BookingTypeHasResource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookingSystem.Models
+{
+    public class BookingTypeHasMeal
+    {
+        [Required(ErrorMessage = "BookingTypeId is required.")]
+        [Range(0, Int16.MaxValue, ErrorMessage = "BookingTypeId is out of range.")]
+        public int BookingTypeId { get; set; }
+
+        [Required(ErrorMessage = "MealId is required.")]
+        [Range(0, Int16.MaxValue, ErrorMessage = "MealId is out of range.")]
+        public int MealId { get; set; }
+
+        // Extra datafields retrieved from database/stored procedure
+        public string BookingTypeName { get; set; }
+
+        public string MealName { get; set; }
+    }
+}
---
+++ BookingSystem/Models/BookingTypeHasMealService.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookingSystem.Models
+{
+    public class BookingTypeHasMealService
+    {
+        // Fields
+        private BookingTypeHasMealDAL _bookingTypeHasMealDAL;
+
+        // Properties
+        private BookingTypeHasMealDAL BookingTypeHasMealDAL
+        {
+            get
+            {
+                return _bookingTypeHasMealDAL ?? (_bookingTypeHasMealDAL = new BookingTypeHasMealDAL());
+            }
+        }
+
+        // Methods
+        public void BookingTypeHasMealDelete(BookingTypeHasMeal BookingTypeHasMeal)
+        {
+            BookingTypeHasMealDelete(BookingTypeHasMeal.BookingTypeId, BookingTypeHasMeal.MealId);
+        }
+
+        public void BookingTypeHasMealDelete(int BookingTypeId, int? MealId = null)
+        {
+            if (BookingTypeId < 0 || MealId != null && MealId < 0)
+            {
+                throw new FormatException("Invalid BookingTypeId or MealId");
+            }
+
+            // Check that the BookingTypeHasMeal exists before deletion
+            BookingTypeHasMeal BookingTypeHasMeal = BookingTypeHasMealDAL.GetBookingTypeHasMealById(BookingTypeId, MealId);
+
+            // If there is no BookingTypeHasMeal
+            if (BookingTypeHasMeal == null)
+            {
+                throw new DataBaseEntryNotFoundException("BookingTypeHasMeal does not exist");
+            }
+
+            // Delete BookingTypeHasMeal
+            BookingTypeHasMealDAL.DeleteBookingTypeHasMeal(BookingTypeId, MealId);
+
+        }
+
+        public BookingTypeHasMeal GetBookingTypeHasMeal(int BookingTypeId)
+        {
+            if (BookingTypeId < 0)
+            {
+                throw new ApplicationException("Invalid BookingTypeId");
+            }
+
+            return BookingTypeHasMealDAL.GetBookingTypeHasMealById(BookingTypeId);
+        }
+
+        public IEnumerable<BookingTypeHasMeal> GetBookingTypeHasMeals(int? BookingTypeId = null)
+        {
+            return BookingTypeHasMealDAL.GetBookingTypeHasMeals(BookingTypeId);
+        }
+
+        public void SaveBookingTypeHasMeal(BookingTypeHasMeal BookingTypeHasMeal)
+        {
+            // Preparare validation return data
+            ICollection<ValidationResult> validationResults;
+
+            // Try to validate given data
+            if (BookingTypeHasMeal.Validate(out validationResults))
+            {
+                // Check that the BookingTypeHasMeal exists
+                BookingTypeHasMeal BookingTypeHasMealCheck = BookingTypeHasMealDAL.GetBookingTypeHasMealById(BookingTypeHasMeal.BookingTypeId, BookingTypeHasMeal.MealId);
+
+                // If a new BookingTypeHasMeal should be created
+                if (BookingTypeHasMealCheck == null)
+                {
+                    BookingTypeHasMealDAL.InsertBookingTypeHasMeal(BookingTypeHasMeal);
+                }
+                // Existing BookingTypeHasMeal should be updated
+                else
+                {
+                    // Update BookingTypeHasMeal
+                    BookingTypeHasMealDAL.UpdateBookingTypeHasMeal(BookingTypeHasMeal);
+                }
+            }
+            // Validation failed
+            else
+            {
+                // Create exception
+                ApplicationException exception = new ApplicationException("BookingTypeHasMeal object contained invalid values.");
+
+                // Add validation data to exception.
+                exception.Data.Add("ValidationResults", validationResults);
+
+                throw exception;
+            }
+        }
+
+        public void SaveBookingTypeHasMeals(BookingTypeHasMeal[] BookingTypeHasMeals)
+        {
+            foreach (BookingTypeHasMeal BookingTypeHasMeal in BookingTypeHasMeals)
+            {
+                this.SaveBookingTypeHasMeal(BookingTypeHasMeal);
+            }
+        }
+    }
+}
