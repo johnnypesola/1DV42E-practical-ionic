852dd7630746d4255bfa2b6fc25b10dbc0d34152
johnnypesola <johnny@pesola.eu>
2016-06-07 19:06:09 +0200

    moved startCtrl to bookingCtrl

--- src/js/controllers/bookings/bookings-ctrl.js
+++ src/js/controllers/bookings/bookings-ctrl.js
@@ -7,37 +7,244 @@ angular.module( 'BookingSystem.bookings',
   )
 
   // Controller
-  .controller( 'BookingListCtrl', [ '$rootScope', '$scope', '$state', 'Booking', '$mdToast', ( $rootScope, $scope, $state, Booking, $mdToast ) => {
+  .controller( 'BookingViewCtrl', [ '$rootScope', '$scope', '$state', 'Booking', 'LocationBooking', 'ResourceBooking', 'MealBooking', '$interval', 'DATA_SYNC_INTERVAL_TIME', '$ionicGesture', '$mdToast', 'DEFAULT_CALENDAR_ZOOM', '$stateParams', '$ionicHistory', '$ionicModal', 'MODAL_ANIMATION', 'BOOKING_TYPES', ( $rootScope, $scope, $state, Booking, LocationBooking, ResourceBooking, MealBooking, $interval, DATA_SYNC_INTERVAL_TIME, $ionicGesture, $mdToast, DEFAULT_CALENDAR_ZOOM, $stateParams, $ionicHistory, $ionicModal, MODAL_ANIMATION, BOOKING_TYPES ) => {
 
     /* Init vars */
+    const modalTemplateUrl = 'templates/modals/booking-create-choose-type.html';
+    const updateIntervalTime = DATA_SYNC_INTERVAL_TIME;
+    let updateInterval = null, weekStartDate = null, weekEndDate = null;
+    $scope.zoom = DEFAULT_CALENDAR_ZOOM;
+    $scope.weekDate = moment();
+    $scope.bookingTypes = BOOKING_TYPES;
+    $scope.bookingsType = $scope.bookingTypes.booking; // $stateParams.bookingType;
 
     /* Private methods START */
 
+    const setupModal = function(){
+
+      $ionicModal.fromTemplateUrl( modalTemplateUrl, {
+        scope: $scope,
+        animation: MODAL_ANIMATION
+      })
+        .then( ( response ) => {
+
+          $scope.modal = response;
+        });
+
+      // Cleanup the modal when we're done with it!
+      $scope.$on( '$destroy', () => {
+        $scope.modal.remove();
+      });
+    };
+
+    const setupWeekStartAndEndDates = function ( offset = 0 ) {
+
+      // Add or subtract offset weeks from current weekdate object.
+      if ( offset > 0 ) {
+        $scope.weekDate = moment( $scope.weekDate ).add( 1, 'weeks' );
+      } else if ( offset < 0 ) {
+        $scope.weekDate = moment( $scope.weekDate ).subtract( 1, 'weeks' );
+      }
+
+      weekStartDate = moment( $scope.weekDate ).startOf( 'week' );
+      weekEndDate = moment( $scope.weekDate ).endOf( 'week' );
+    };
+
+    const setCalendarTitle = function () {
+
+      switch ( $scope.bookingsType ) {
+        case $scope.bookingTypes.booking:
+          $scope.calendarTitle = 'Bokningstillfällen';
+          break;
+        case $scope.bookingTypes.location:
+          $scope.calendarTitle = 'Lokalbokningar';
+          break;
+        case $scope.bookingTypes.resource:
+          $scope.calendarTitle = 'Resursbokningar';
+          break;
+        case $scope.bookingTypes.meal:
+          $scope.calendarTitle = 'Måltidsbokningar';
+          break;
+        default:
+          $scope.calendarTitle = 'Bokningstillfällen';
+      }
+    };
+
     const getBookings = function () {
 
-      const bookings = Booking.query();
+      let bookings;
 
-      // In case booking cannot be fetched, display an error to user.
+      switch ( $scope.bookingsType ) {
+
+        // If its a booking
+        case $scope.bookingTypes.booking:
+
+          bookings = Booking.queryLessForPeriod({
+            fromDate: weekStartDate.format( 'YYYY-MM-DD' ),
+            toDate: weekEndDate.format( 'YYYY-MM-DD' )
+          });
+          break;
+
+        // If its a location booking
+        case $scope.bookingTypes.location:
+
+          bookings = LocationBooking.queryLessForPeriod({
+            fromDate: weekStartDate.format( 'YYYY-MM-DD' ),
+            toDate: weekEndDate.format( 'YYYY-MM-DD' )
+          });
+          break;
+
+        // If its a resource booking
+        case $scope.bookingTypes.resource:
+
+          bookings = ResourceBooking.queryLessForPeriod({
+            fromDate: weekStartDate.format( 'YYYY-MM-DD' ),
+            toDate: weekEndDate.format( 'YYYY-MM-DD' )
+          });
+          break;
+
+        // If its a meal booking
+        case $scope.bookingTypes.meal:
+          bookings = MealBooking.queryLessForPeriod({
+            fromDate: weekStartDate.format( 'YYYY-MM-DD' ),
+            toDate: weekEndDate.format( 'YYYY-MM-DD' )
+          });
+          break;
+
+        default:
+          bookings = {};
+      }
+
+      // const bookings = Booking.query();
+
+      // In case Booking cannot be fetched, display an error to user.
       bookings.$promise.catch( () => {
 
         $mdToast.show( $mdToast.simple()
-          .content( 'Bokningstillfällen kunde inte hämtas, var god försök igen.' )
-          .position( 'top right' )
+            .content( 'Bokningar kunde inte hämtas, var god försök igen.' )
+            .position( 'top right' )
         );
-      });
+      })
 
-      $scope.bookings = bookings;
+        .then( ( bookings ) => {
+          $scope.bookings = bookings;
+        });
+    };
+
+    const startUpdateInterval = function (){
+
+      updateInterval = $interval( () => {
+
+        getBookings();
+
+      }, updateIntervalTime );
+
+    };
+
+    const setupGestures = function() {
+
+      const element = angular.element( document.querySelector( '#booking-view-content' ) );
+
+      $ionicGesture.on( 'pinch', ( e ) => {
+
+        e.gesture.srcEvent.preventDefault();
+
+        $scope.$apply( () => {
+
+          // Change zoom value after pinch value.
+          $scope.zoom = e.gesture.scale;
+        });
+
+      }, element );
+
+      $ionicGesture.on( 'swipeleft', ( e ) => {
+
+        $scope.toNextWeek();
+
+        $scope.$apply();
+
+        // Broadcast to children that a swipe occured
+        $scope.$broadcast( 'swipe-occurred' );
+
+      }, element );
+
+      $ionicGesture.on( 'swiperight', ( e ) => {
+
+        $scope.toPreviousWeek();
+
+        $scope.$apply();
+
+        // Broadcast to children that a swipe occured
+        $scope.$broadcast( 'swipe-occurred' );
+
+      }, element );
     };
 
     /* Private Methods END */
 
     /* Public Methods START */
 
+    $scope.toNextWeek = function() {
+      setupWeekStartAndEndDates( 1 );
+
+      getBookings();
+    };
+
+    $scope.toPreviousWeek = function() {
+      setupWeekStartAndEndDates( -1 );
+
+      getBookings();
+    };
+
+    $scope.changeBookingTypeTo = function( bookingTypeStr ) {
+
+      // We are actually in parent scope at the moment. Therefore we need to broadcast.
+      $scope.$broadcast( 'changed-booking-type', { bookingType: bookingTypeStr });
+
+    };
+
+    $scope.createBookingOfType = function( bookingTypeStr ) {
+
+      // Redirect to edit view
+      $state.go( 'app.' + bookingTypeStr + '-create' );
+
+      $scope.modal.hide();
+    };
+
     /* Public Methods END */
 
     /* Initialization START */
-    $scope.$on( '$ionicView.enter', ( event, data ) => {
+
+    $scope.$on( 'changed-booking-type', ( event, args ) => {
+
+      $scope.bookingsType = args.bookingType;
       getBookings();
+    });
+
+    $scope.$on( '$ionicView.enter', ( event, data ) => {
+
+      setupWeekStartAndEndDates();
+      getBookings();
+      startUpdateInterval();
+      setCalendarTitle();
+      setupModal();
+    });
+
+    $scope.$on( '$ionicView.loaded', ( event, data ) => {
+
+      setupGestures();
+    });
+
+    // Destroy the update interval when controller is destroyed (when we leave this view)
+    $scope.$on( '$ionicView.leave', ( event ) => {
+
+      // Cancel local update interval
+      if ( updateInterval ) {
+        $interval.cancel( updateInterval );
+      }
+
+      // Broadcast to children to cancel update intervals
+      $scope.$broadcast( 'leaving-view' );
     });
 
     /* Initialization END */
