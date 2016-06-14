4f30251686518ea4a06b7baddb07244af3bd3da5
johnnypesola <johnny@pesola.eu>
2016-06-14 15:54:51 +0200

    customer validation complete

--- src/js/app.js
+++ src/js/app.js
@@ -65,7 +65,7 @@ BookingSystem.run( ['$ionicPlatform', '$rootScope', ( $ionicPlatform, $rootScope
   moment.locale( 'sv' );
 
   // Global REGEXP
-  $rootScope.TEXT_FIELD_REGEXP = /^[a-zA-ZåäöÅÄÖé\-_&\.,~\^@()/%\s\!]*$/;
+  $rootScope.TEXT_FIELD_REGEXP = /^[0-9a-zA-ZåäöÅÄÖé\-_&\.,~\^@()/%\s\!]*$/;
   $rootScope.NUMERIC_SPACE_REGEXP = /^[0-9\s\+]*$/;
 
 }] );
