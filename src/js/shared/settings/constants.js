1f3852c47a2602996a70ca1d6430ffd0e4ca8b35
johnnypesola <johnny@pesola.eu>
2016-06-30 17:33:59 +0200

    Refactoring and added basic auth code

---
+++ src/js/shared/settings/constants.js
@@ -0,0 +1,22 @@
+angular.module( 'BookingSystem.constants',
+
+  []
+)
+  .constant( 'API_URL', 'http://bokning.vvfors.se/api/' )
+  //.constant( 'API_URL', 'http://localhost:8706/api/' );
+  .constant( 'API_IMG_PATH_URL', 'http://bokning.vvfors.se/' )
+  .constant( 'UPLOAD_IMG_MAX_WIDTH', '300' )
+  .constant( 'UPLOAD_IMG_MAX_HEIGHT', '300' )
+  .constant( 'PHOTO_MISSING_SRC', 'img/photo_missing.svg' )
+  .constant( 'DEFAULT_MAP_ZOOM', 5 )
+  .constant( 'DEFAULT_LATITUDE', 67.2792 )
+  .constant( 'DEFAULT_LONGITUDE', 4.2361 )
+  .constant( 'MODAL_ANIMATION', 'slide-in-up' )
+  .constant( 'DATA_SYNC_INTERVAL_TIME', 60000 * 5 ) // Every 5 minutes
+  .constant( 'DEFAULT_CALENDAR_ZOOM', 2 )
+  .constant( 'BOOKING_TYPES', {
+    booking : 'booking',
+    location : 'location-booking',
+    resource : 'resource-booking',
+    meal : 'meal-booking'
+  });
---
+++ src/js/shared/settings/ng-material-settings.js
@@ -0,0 +1,42 @@
+angular.module( 'BookingSystem.ngMaterialSettings',
+
+  ['ngMaterial']
+)
+
+// Localization configuration for Angular Material ( Swedish localization. )
+.config( ['$mdDateLocaleProvider', '$mdGestureProvider', ( $mdDateLocaleProvider, $mdGestureProvider ) => {
+
+  $mdDateLocaleProvider.months = ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December'];
+  $mdDateLocaleProvider.shortMonths = ['jan', 'feb', 'mar', 'apr', 'maj', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'];
+  $mdDateLocaleProvider.days = ['söndag', 'måndag', 'tisdag', 'onsdag', 'torsdag', 'fredag', 'lördag'];
+  $mdDateLocaleProvider.shortDays = ['Sö', 'Må', 'Ti', 'On', 'To', 'Fr', 'Lö'];
+
+  // Can change week display to start on Monday.
+  $mdDateLocaleProvider.firstDayOfWeek = 1;
+
+  // Example uses moment.js to parse and format dates.
+  $mdDateLocaleProvider.parseDate = function( dateString ) {
+    const m = moment( dateString, 'L', true );
+    return m.isValid() ? m.toDate() : new Date( NaN );
+  };
+  $mdDateLocaleProvider.formatDate = function( date ) {
+    return moment( date ).format( 'L' );
+  };
+  $mdDateLocaleProvider.monthHeaderFormatter = function( date ) {
+    return $mdDateLocaleProvider.shortMonths[date.getMonth()] + ' ' + date.getFullYear();
+  };
+  // In addition to date display, date components also need localized messages
+  // for aria-labels for screen-reader users.
+  $mdDateLocaleProvider.weekNumberFormatter = function( weekNumber ) {
+    return 'Vecka ' + weekNumber;
+  };
+  $mdDateLocaleProvider.msgCalendar = 'Kalender';
+  $mdDateLocaleProvider.msgOpenCalendar = 'Öppna kalender';
+}]
+)
+
+// Fix for double triggering of ng-click, caused by angular material design.
+.config( ['$mdGestureProvider', ( $mdGestureProvider ) => {
+  $mdGestureProvider.skipClickHijack();
+}]
+);
